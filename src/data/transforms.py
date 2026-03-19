b198b6eae949983a2ad89e442acc49ccf39b6dbf
Richard <>
2026-03-19 12:17:12 +0100

    perf: transforms.py location

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -3,8 +3,11 @@ import torchvision.transforms.v2 as v2
 
 
 class ToTensor(torch.nn.Module):
+    """
+    Equivalent to transforms v1 ToTensor.
+    """
+
     def forward(self, img, label=None):
-        # Do some transformations
         if label is not None:
             return (
                 v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)])(img),
