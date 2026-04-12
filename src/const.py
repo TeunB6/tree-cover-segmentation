5140d4d893059dca8e627bf135a760e0a5461313
TeunB06 <t.boersma.4@student.rug.nl>
2026-04-12 13:42:55 +0200

    fix: undo erroneous commit of adapted habrok specific filepath

--- src/const.py
+++ src/const.py
@@ -4,7 +4,7 @@ from rich.console import Console
 import torch
 
 DATA_URL = "https://owncloud.gwdg.de/index.php/s/H6MsR0wVGRuPPl3/download"
-DATA_PATH = Path("/scratch/s5195179/tree_detection_data")
+DATA_PATH = Path("/data")
 DATA_HASH = "0ad723302caa8f34e3e9a451ce76973a399fb25035e5c244b490f606f3d7dbb9"
 NEON_TREE_PATH = DATA_PATH / "neon_tree" / "NeonTreeEvaluation"
 PT_DATA_PATH = DATA_PATH / "pt_data"
